b876706ee91b256652ae33d02d49ca05f9216c44
AnviVerma27 <anviver2005@gmail.com>
2023-07-31 01:06:27 +0530

    changes

---
+++ helper.py
@@ -0,0 +1,49 @@
+import streamlit as st
+import base64 
+from datetime import date 
+import time 
+import cv2 
+
+def set_bg_hack():
+    main_bg = "background.png"
+    main_bg_ext = "png"
+    
+    bg="backg.jpg"
+    bg_ext = "jpg"
+        
+    st.markdown(
+         f"""
+         <style>
+         .appview-container {{
+             background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()})
+         }}
+         .block-container {{
+             background: url(data:image/{bg_ext};base64,{base64.b64encode(open(bg, "rb").read()).decode()})
+         }}
+         </style>
+         """,
+         unsafe_allow_html=True
+     )
+        
+def header1(text):
+    html_temp = f"""
+    <h1 style = "color:brown; text_align:center; font-weight: bold;"> {text} </h2>
+    </div>
+    """   
+    st.markdown(html_temp, unsafe_allow_html = True)
+    
+def header2(text):
+    html_temp = f"""
+    <h3 style = "color:brown; text_align:center; font-weight: bold;"> {text} </h2>
+    </div>
+    """   
+    st.markdown(html_temp, unsafe_allow_html = True)
+
+def header3(text):
+    html_temp = f"""
+    <h5 style = "color:brown; text_align:center; font-weight: bold;"> {text} </h2>
+    </div>
+    """   
+    st.markdown(html_temp, unsafe_allow_html = True)
+    
+    
--- main.py
+++ main.py
@@ -4,14 +4,32 @@ import pandas as pd
 import torch
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
+import helper as help
+from elevenlabs import generate, play
 
 model = joblib.load('transformer.joblib')
 embeddings = joblib.load('embeddings.pkl')
 sentences = joblib.load('sentences.pkl')
 
+st.set_page_config(page_title ="TalkingRecipeBook", layout='centered')
+
+help.set_bg_hack()
+
 df = pd.read_parquet('final_recipes.parquet')
 
-st.title('Enter the Dish...')
+title="""<center> Talking recipe book </center>"""
+
+help.header1(title)
+
+text = """
+    <center> <br> Welcome to the talking recipe book. </br> </center>
+    <center> This app will help you in making new dishes everyday.</center>
+    <center> It has an inbuilt voice feature that will recite the whole recipe to you for your ease</center>
+    """
+help.header3(text)
+
+help.header2("Enter the dish.....")
+
 paper_name = st.text_input("Input here")
 
 def predict():
@@ -22,7 +40,28 @@ def predict():
     top_dish = np.argsort(cosine_scores, axis=1)[0][-1:][::-1]
 
     for i in top_dish:
-        st.write(df[df['title'] == sentences[i]]['directions'].values[0])
+        str=df[df['title'] == sentences[i]]['directions'].values[0]
+        s=str.replace('[','')
+        t=s.replace(']','')
+        str1=t.replace('"','')
+        list=str1.split(',')
+        paragraph_length = 1
+        ing=df[df['title'] == sentences[i]]['ingredients'].values[0]
+        help.header3(ing)
+        paragraphs = [list[i:i+paragraph_length] for i in range(0, len(list), paragraph_length)]
+        for paragraph in paragraphs:
+            paragraph_text = ','.join(paragraph)
+            st.write(paragraph)
+            text_to_speech(paragraph_text)
+
+def text_to_speech(text):
+    audio = generate(
+    text=text,
+    voice="Bella",
+    model='eleven_monolingual_v1',
+    api_key='78e907ae7b2a82979ff65e285400bf07'
+    )
+    play(audio)
 
 if st.button('Display'):
     predict()
